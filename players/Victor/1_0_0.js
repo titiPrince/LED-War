e05568e13e08a4cd528910c5ed9a562f1229072f
Evan Fortin <evan.fortin@epitech.eu>
2024-04-18 21:53:45 +0200

    Better player script interface

--- players/Victor/1_0_0.js
+++ players/Victor/1_0_0.js
@@ -1,70 +1,96 @@
-let utils_board = {
+let gameState = {
     board: [],
     width: undefined,
     height: undefined,
-    playerCount: 2,
+    playerCount: undefined,
+    turn: undefined,
     me: {
         id: undefined,
         x: undefined,
         y: undefined,
-        energy: undefined
+        energy: undefined,
+
+        goUp: "UP",
+        goDown: "BOTTOM",
+        goLeft: "LEFT",
+        goRight: "RIGHT",
+
+        canGoUp: () => gameState.me.y > 0,
+        canGoDown: () => gameState.me.y < gameState.height - 1,
+        canGoLeft: () => gameState.me.x > 0,
+        canGoRight: () => gameState.me.x < gameState.width - 1,
+
+        getUpTile: () => gameState.board[gameState.me.y - 1 * gameState.width + gameState.me.x],
+        getDownTile: () => gameState.board[gameState.me.y + 1 * gameState.width + gameState.me.x],
+        getLeftTile: () => gameState.board[gameState.me.y * gameState.width + gameState.me.x - 1],
+        getRightTile: () => gameState.board[gameState.me.y * gameState.width + gameState.me.x + 1],
+
+        getOtherPlayers: () => gameState.board.filter(tile => gameState.tile.isPlayer(tile) && tile.id !== gameState.me.id),
+
+        goTo: (x, y) => {
+            if (gameState.me.x < x) return gameState.me.goRight;
+            if (gameState.me.x > x) return gameState.me.goLeft;
+            if (gameState.me.y < y) return gameState.me.goDown;
+            if (gameState.me.y > y) return gameState.me.goUp;
+            return null;
+        }
+    },
+    tile: {
+        WALL: 0,
+        PLAYER_DRAG: 1,
+        EMPTY: 2,
+
+        isTile: (tile) => tile.type === "tile",
+        isPlayer: (tile) => tile.type === "player",
+
+        isWall: (tile) => gameState.tile.isTile(tile) && tile.category === gameState.tile.WALL,
+        isEmpty: (tile) =>gameState.tile.isTile(tile) && tile.category === gameState.tile.EMPTY,
+        isPlayerDrag: (tile) => gameState.tile.isTile(tile) && tile.category === gameState.tile.PLAYER_DRAG,
+
+        isMe: (tile) => gameState.tile.isPlayer(tile) && tile.id === gameState.me.id
     }
 };
 
-function utils() {
+function getGameTurnData() {
     const changes = global.game;
+
+    if (gameState.turn === undefined) gameState.turn = 0;
+    else gameState.turn++;
 
     for (const change of changes) {
         if (change.type === 'info') {
-            utils_board.width = change.width;
-            utils_board.height = change.height;
-            utils_board.playerCount = change.playerCount;
-            utils_board.me.id = change.playerId;
+            gameState.width = change.width;
+            gameState.height = change.height;
+            gameState.playerCount = change.playerCount;
+            gameState.me.id = change.playerId;
+
+            gameState.board = new Array(gameState.width * gameState.height);
         }
         else {
             let x = change.x;
             let y = change.y;
 
-            let index = y * utils_board.width + x;
+            let index = y * gameState.width + x;
 
-            utils_board.board[index] = change;
+            gameState.board[index] = change;
 
-            if (change.type === 'player') {
-                if (change.id === utils_board.me.id) {
-                    utils_board.me.x = x;
-                    utils_board.me.y = y;
-                    utils_board.me.energy = change.energy;
-                }
+            if (change.type === 'player' && change.id === gameState.me.id) {
+                gameState.me.x = x;
+                gameState.me.y = y;
+                gameState.me.energy = change.energy;
             }
         }
     }
 
-    return utils_board;
+    return gameState;
 }
 
 function main() {
-    let data = utils();
+    let data = getGameTurnData();
+    const me = data.me;
 
-    const canGoUp = data.me.y > 0;
-    const canGoDown = data.me.y < data.height - 1;
-    const canGoLeft = data.me.x > 0;
-    const canGoRight = data.me.x < data.width - 1;
+    const players = me.getOtherPlayers();
+    const target = players[0];
 
-    const move = Math.floor(Math.random() * 4);
-
-    if (data.me.id === 0) {
-        if (data.me.y > 2) return 'UP';
-        if (data.me.x > 2) return 'LEFT';
-    }
-    else if (data.me.id === 1) {
-        if (data.me.y < 27) return 'BOTTOM';
-        if (data.me.x < 27) return 'RIGHT';
-    }
-
-    if (move === 0 && canGoUp) return 'UP';
-    if (move === 1 && canGoDown) return 'BOTTOM';
-    if (move === 2 && canGoLeft) return 'LEFT';
-    if (move === 3 && canGoRight) return 'RIGHT';
-
-    return 'UP';
+    return me.goTo(target.x, target.y);
 }
